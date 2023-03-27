d54a32de899196bb87d61aeb0f8512e08bf1c35e
k.tukhtasinov <k.tukhtasinov@fdmgroup.com>
2023-03-27 20:59:25 +0200

    First task

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        int number = 10;
+        int sum = Solution.solution(number);
+        System.out.println(sum);
+    }
+}
+
