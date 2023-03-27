ad923c8bedd7c8867aeef455f60f6f173f32c711
k.tukhtasinov <k.tukhtasinov@fdmgroup.com>
2023-03-27 21:36:11 +0200

    Second task

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,9 +2,14 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        //First Task
         int number = 10;
         int sum = Solution.solution(number);
         System.out.println(sum);
+        //Second Task
+        int seconds = 4526;
+        String result = HumanReadableTime.makeReadable(seconds);
+        System.out.println(result);
     }
 }
 
